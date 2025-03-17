a64995474918a4f977f040319a6fe1a316e4711c
A1123317 <a1123317@mail.nuk.edu.tw>
2025-03-17 21:22:34 +0800

    Add files via upload

---
+++ info.php
@@ -0,0 +1,15 @@
+<?php
+
+$name=$_GET["name"];
+$id=$_GET["id"];
+$diet=$_GET=["diet"];
+$grade=$_GET["grade"];
+
+
+
+echo "姓名 :"."$name"."<br>";
+echo "學號 :"."$id"."<br>";
+echo "飲食 :"."$diet"."<br>";
+echo "年級 :""$grade"."<br>";
+
+?>
